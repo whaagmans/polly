f5ecfdc9f17b9a52a56c699c397577a21a41cdd9
Redeamer <wehaagmans@gmail.com>
2023-05-11 18:28:49 +0200

    chore: :memo: Variable name change to make the varialbe usage easier to understand

--- src/types/Modal.ts
+++ src/types/Modal.ts
@@ -1,6 +1,6 @@
 import { ModalSubmitInteraction } from 'discord.js';
 
 export interface Modal {
-	name: string;
+	modalReference: string;
 	run: (interaction: ModalSubmitInteraction) => Promise<void>;
 }
