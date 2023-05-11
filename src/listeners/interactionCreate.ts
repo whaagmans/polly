66ac263a94874f0494774fead51e2cf4b793f84b
Redeamer <wehaagmans@gmail.com>
2023-05-11 18:25:21 +0200

    refactor: :fire: Removed old way of handling modals

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -1,36 +1,12 @@
-import {
-	Client,
-	CommandInteraction,
-	Events,
-	Interaction,
-	ModalSubmitInteraction,
-} from 'discord.js';
-import { Commands } from 'src/Commands';
-import { Modals } from 'src/Modals';
+import { Client, CommandInteraction, Events, Interaction } from 'discord.js';
+import { Commands } from '../Commands';
 
 const interactionCreate = (client: Client): void => {
 	client.on(Events.InteractionCreate, async (interaction: Interaction) => {
 		if (interaction.isCommand() || interaction.isContextMenuCommand()) {
 			await handleSlashCommand(interaction);
 		}
-		if (interaction.isModalSubmit()) {
-			await handleModalSubmit(interaction);
-		}
 	});
-};
-
-const handleModalSubmit = async (
-	interaction: ModalSubmitInteraction
-): Promise<void> => {
-	const modalResponse = Modals.find(
-		(modal) => modal.name === interaction.customId
-	);
-	if (!modalResponse) {
-		interaction.followUp({ content: 'an error has occured', ephemeral: true });
-		return;
-	}
-
-	modalResponse.run(interaction);
 };
 
 const handleSlashCommand = async (
